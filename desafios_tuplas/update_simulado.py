748e922456140ca68892ad71e0027920303eda44
debora-oliv <deboraalves007@hotmail.com>
2026-04-27 20:27:53 -0300

    remove: deletar exercicio tupla

--- desafios_tuplas/update_simulado.py
+++
@@ -1,3 +0,0 @@
-"""
-Recebe uma tupla que guarda as configurações de um usuário no formato (id_usuario, tema, notificacoes_ativas) e retorna uma nova tupla reaproveitando os dados da tupla original com atualização apenas do tema.
-"""
