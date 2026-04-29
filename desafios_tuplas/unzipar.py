36734fb8c6d671b1e8991339b4aadbad6e18e72c
Débora <deboraalves007@hotmail.com>
2026-04-28 23:39:49 -0300

    delete: desafios_tuplas/unzipar.py

--- desafios_tuplas/unzipar.py
+++
@@ -1,3 +0,0 @@
-"""
-Recebe uma lista de tuplas emparelhadas representando no formato (Produto, Preço) e "descompacta" em duas tuplas separadas: uma só com os nomes dos produtos e outra só com os preços correspondentes.
-"""
